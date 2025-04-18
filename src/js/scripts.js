a54cd7be283b4bdf9d344762f53bcfe853a08d7c
Samantha Koziar <koziar@ualberta.ca>
2025-04-18 17:11:49 -0600

    initial commit - add start of constructors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,24 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  
+  class Engine {
+    constructor(CylinderCount) {
+      this.CylinderCount = 6;
+      this.isRunning = isRunning;
+    }
   }
+  
+  
+  class Car { 
+    constructor(make, model, year) {
+      this.make = make || "Lexus";
+      this.model = model || "IS 300";
+      this.year = 2002;
+      this.odometer = 0;
+
+    }
+
+}
+
 }
