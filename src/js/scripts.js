6d0f4af8395ee8c099a022aebf87c48457bf0540
Samantha Koziar <koziar@ualberta.ca>
2025-04-18 18:34:00 -0600

    add startEngine, stopEngine, and drive method

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,35 +2,60 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  
+ 
+ 
+  //Engine class
   class Engine {
     constructor() {
       this.CylinderCount = 6;
-      this.isRunning = false; // car start is off. On = true
+      this.isRunning = false; // Engine starts off
     }
   }
-  
-  
-  class Car { 
+
+  // Car class
+  class Car {
     constructor(make, model, year) {
       this.make = make || "Lexus";
       this.model = model || "IS 300";
-      this.year = 2002;
+      this.year = year || 2002;
       this.odometer = 0;
+      this.engine = new Engine(); // Attach an engine
+    }
+
+    // Start engine method
+    startEngine() {
+      this.engine.isRunning = true;
+      output(`Engine has been started`);
+    }
+
+    // Stop engine method
+    stopEngine() {
+      this.engine.isRunning = false;
+      output(`Engine has been turned off`);
+    }
+
+    // Drive method
+    drive(kilometers) {
+      if (this.engine.isRunning) {
+        this.odometer += kilometers;
+        output(`Odometer is currently: ${this.odometer}km`);
+      } else {
+        output("The engine is off.");
+      }
     }
   }
 
+// Start sequence as per instructions in assignment
 
-let myCar = new Car("Lexus", "IS300", 2002); // 1. Instantiate car
+  let myCar = new Car("Lexus", "IS300", 2002); // 1. Instantiate car
 
-myCar.startEngine(); // 2. Turn its engine on
-myCar.drive(100); // 3. Drive for 100km 
-myCar.stopEngine(); // 4. Turn the engine off
-myCar.startEngine(); // 5. Turn its engine on
-myCar.drive(50); // 6. Drive for 50km
-myCar.startEngine(); // 7. Turn the engine off
+  myCar.startEngine();     // 2. Turn its engine on
+  myCar.drive(100);        // 3. Drive for 100km
+  myCar.stopEngine();      // 4. Turn the engine off
+  myCar.startEngine();     // 5. Turn its engine on
+  myCar.drive(50);         // 6. Drive for 50km
+  myCar.stopEngine();      // 7. Turn the engine off
 
-output("Output of odomoter reading here"); // Output the odometer reading to the console 
-
+  output(`Final odometer reading: ${myCar.odometer}km`); // 8.	Output the odometer reading to the console.
 
 }
